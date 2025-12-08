6d9ad06a2faf780c74a7db7397932a46ce5109db
plastic-melter <140357149+plastic-melter@users.noreply.github.com>
2025-12-08 00:34:08 -0800

    pre-build: 2025-12-08 00:34:08

--- dotfiles/neovim/lua/keymaps.lua
+++ dotfiles/neovim/lua/keymaps.lua
@@ -3,6 +3,10 @@ vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Fin
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Buffers' })
 
+-- Don't add extra indent with o/O
+vim.keymap.set('n', 'o', 'o<Esc>S', { desc = 'Open line without auto-indent' })
+vim.keymap.set('n', 'O', 'O<Esc>S', { desc = 'Open line above without auto-indent' })
+
 -- Copy to clipboard with Ctrl-Space
 vim.keymap.set('n', '<C-Space>', function()
   vim.fn.system('wl-copy', vim.fn.getreg('"'))
