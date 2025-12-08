93947ec1428e4f9ccfd480ce9cd6297d64a1a0b2
plastic-melter <140357149+plastic-melter@users.noreply.github.com>
2025-12-08 02:41:47 -0800

    flake update: 2025-12-08 02:41:47

--- home.nix
+++ home.nix
@@ -11,7 +11,7 @@ home = {
 };
 
 # ============================================
-# DOTFILES CONFIGURATION
+# DOTFILES TO PULL INTO ~/.config
 # ============================================
 
 home.file = {
